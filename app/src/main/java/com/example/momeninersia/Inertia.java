b87d1caea88f15d9d4600a337ffcf4044ef56a24
“Ishak4875” <ishakjaelani07@gmail.com>
2023-03-11 07:53:13 +0800

    update layout and logic

--- app/src/main/java/com/example/momeninersia/Inertia.java
+++ app/src/main/java/com/example/momeninersia/Inertia.java
@@ -4,7 +4,7 @@ package com.example.momeninersia;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class Inertia implements Parcelable {
+public class Inertia{
     private String title;
     private String formula;
     private Integer avatar;
@@ -12,28 +12,6 @@ public class Inertia implements Parcelable {
     Inertia(){
 
     }
-
-    protected Inertia(Parcel in) {
-        title = in.readString();
-        formula = in.readString();
-        if (in.readByte() == 0) {
-            avatar = null;
-        } else {
-            avatar = in.readInt();
-        }
-    }
-
-    public static final Creator<Inertia> CREATOR = new Creator<Inertia>() {
-        @Override
-        public Inertia createFromParcel(Parcel in) {
-            return new Inertia(in);
-        }
-
-        @Override
-        public Inertia[] newArray(int size) {
-            return new Inertia[size];
-        }
-    };
 
     public String getTitle() {
         return title;
@@ -57,22 +35,5 @@ public class Inertia implements Parcelable {
 
     public void setAvatar(Integer avatar) {
         this.avatar = avatar;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(title);
-        parcel.writeString(formula);
-        if (avatar == null) {
-            parcel.writeByte((byte) 0);
-        } else {
-            parcel.writeByte((byte) 1);
-            parcel.writeInt(avatar);
-        }
     }
 }
